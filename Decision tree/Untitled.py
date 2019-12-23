d7c478d042052837bbcb4ab9bb172dfc6d6560e6
Keycat <owo.nthu@outlook.com>
2019-12-23 23:06:02 +0800

    決策樹：網頁狀態預測 	資料讀取 	資料清洗 	資料分割 	訓練模型(by pyspark.mllib) 下一步目標： 	不使用mllib，只用mapreduce來改寫

---
+++ Decision tree/Untitled.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# import findspark
+# findspark.init()
+
+import glob # to get file paths
+import random
+import numpy as np # only to get prime number list
+import itertools # to generate pairs from list
+
+import pyspark
+from pyspark import SparkConf, SparkContext
+
+
+# In[ ]:
+
+
+
+
